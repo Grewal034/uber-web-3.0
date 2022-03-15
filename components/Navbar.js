fa6757595048e0a353458dfcda6fa1029774e012
Ricky Grewal <Rickygrewal@protonmail.com>
2022-03-14 19:27:40 -0600

    changed from template to nothingness

---
+++ components/Navbar.js
@@ -0,0 +1,8 @@
+const style = {
+  wrapper: 'h-16 w-full bg-black text-white flex md:justify-around items-center px-60'
+}
+const Navbar = () => {
+  return <div className={Style.wrapper}>Navbar</div> 
+}
+
+export default Navbar
