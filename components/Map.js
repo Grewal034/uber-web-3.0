ec0ef0190fcff8532985896664a259ded4106f2e
Ricky Grewal <Rickygrewal@protonmail.com>
2022-03-15 00:44:56 -0600

    ✅

--- components/Map.js
+++ components/Map.js
@@ -14,8 +14,8 @@ const Map = () => {
   useEffect(() => {
     const map = new mapboxgl.Map({
       container: 'map',
-      style: 'mapbox://styles/drakosi/ckvcwq3rwdw4314o3i2ho8tph',
-      center: [-99.29011, 39.39172],
+      style: 'mapbox://styles/mrgwall/ckq3lorj00jz717nrwxymplxa',
+      center: [43.6569, -79.4049],
       zoom: 3,
     })
 
